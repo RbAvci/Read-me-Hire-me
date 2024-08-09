da19f1d3afbd33470f4688df99228ec1cf330429
fikretellek <fikretellek@icloud.com>
2024-08-09 21:19:06 +0100

    query added to get list of graduates

--- server/controller/getGradList.js
+++ server/controller/getGradList.js
@@ -2,28 +2,35 @@ import db from "../db";
 
 export default async function getGradList(req, res, next) {
 	try {
-		const user_id_check = await db.query(
-			`SELECT COUNT(*) as count FROM projects WHERE user_id = $1`,
-			[user_id]
+		const userListData = await db.query(
+			`
+            SELECT
+                u.id AS user_id,
+                COUNT(DISTINCT p.id) AS project_count,
+                COUNT(DISTINCT pr.id) AS pull_request_count,
+                ARRAY_AGG(pr.created_at) AS pr_dates_array,
+                prof.avatar,
+                u.github_username
+            FROM
+                users u
+            LEFT JOIN
+                projects p ON u.id = p.user_id
+            LEFT JOIN
+                pull_requests pr ON u.id = pr.user_id
+            LEFT JOIN
+                profiles prof ON u.id = prof.user_id
+            WHERE
+                u.user_type = 'graduate'
+            GROUP BY
+                u.id, prof.avatar, u.github_username;
+            `
 		);
 
-		if (user_id_check.rows[0].count == 0) {
-			return res.status(404).json({ error: "User not found" });
+		if (userListData.rows.length == 0) {
+			return res.status(404).json({ error: "No user data found" });
 		}
 
-		const latestTimestampResult = await db.query(
-			`SELECT MAX(fetch_time) AS latest_timestamp FROM projects WHERE user_id = $1`,
-			[user_id]
-		);
-
-		const latestTimestamp = latestTimestampResult.rows[0].latest_timestamp;
-
-		const projects_data = await db.query(
-			`SELECT * FROM projects WHERE user_id = $1 AND fetch_time = $2`,
-			[user_id, latestTimestamp]
-		);
-
-		res.status(200).json({ projects: projects_data.rows });
+		res.status(200).json({ graduateList: userListData.rows });
 	} catch (error) {
 		res.status(500).json({ error: "Internal server error" });
 	}
